6ac9c447c8c1d318f7d96c69cbe2978795d4bd1a
Arintia <arantiabusiness@gmail.com>
2022-04-28 16:46:42 +0300

    Add initial code for room design

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -5,8 +5,9 @@ export default function Room() {
     return (
         <div className="w-screen h-screen bg-slate-100 overflow-hidden">
             <Navbar />
-            
-
+            <main className="w-screen h-full flex md:flex-row flex-col justify-evenly items-center">
+                s
+            </main>
             <Footer />
         </div>
     );
